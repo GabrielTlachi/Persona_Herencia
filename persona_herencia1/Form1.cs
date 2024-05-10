f36517e6c2c36d080346bfd6dd96a5caf89f863d
GabrielTlachi <163442595+GabrielTlachi@users.noreply.github.com>
2024-05-09 19:03:45 -0600

    Add files via upload

---
+++ persona_herencia1/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace persona_herencia1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void limpiar_Click(object sender, EventArgs e)
+        {
+            //Aqui borramos todo el texto que hubieramos puesto
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            textBox6.Clear();
+        }
+
+        private void guardar_Click(object sender, EventArgs e)
+        {
+            //Aqui se encuentra el boton para guardar datos
+            MessageBox.Show("DATOS GUARDADOS");
+        }
+
+        private void Salir_Click(object sender, EventArgs e)
+        {
+            //Aqui cerramos el programa
+            Close();
+        }
+    }
+}
---
+++ persona_herencia1/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace persona_herencia1
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+
+    //clase papá es la cabeza de la clase
+    public class Persona
+    {
+        //String=Texto Int=Numero Entero
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+
+        //Get y Set es para tomar y dar datos
+        public string   Nacimiento { get; set; }
+        public Persona(string nombre, int edad, string nacimiento)
+        {
+            //Caja de texto y afectara las clases posteriores
+            Nombre = nombre;
+            Edad = edad;
+            Nacimiento = nacimiento;
+        }
+        
+    }
+
+    //clase hija 
+    //Aqui se podra ver los diferentes tipos de carga de personas Aluno, Empleado y Docente
+    public class Alumno
+    {
+        public string Carrera { get; set; }
+        public int Matricula { get; set; }
+
+
+        public Alumno(string nombre, int edad, string nacimiento, string carrera, int matricula)
+        {
+            
+            Carrera = carrera;
+            Matricula = matricula;
+
+        }
+
+    }
+
+    //clase hija
+    public class Empleado
+    {
+
+        public string Carrera { get; set; }
+        public int Matricula { get; set; }
+
+        public int Sueldo { get; set; }
+
+        public Empleado(string nombre, int edad, string nacimiento, string carrera, int matricula, int sueldo)
+        {
+
+            Carrera = carrera;
+            Matricula = matricula;
+            Sueldo = sueldo;
+        }
+
+    }
+
+    //clase hija
+    public class Docente
+    {
+
+        public string Carrera { get; set; }
+        public int Matricula { get; set; }
+
+        public int Sueldo { get; set; }
+
+        public Docente(string nombre, int edad, string nacimiento, string carrera, int matricula, int sueldo)
+        {
+
+            Carrera = carrera;
+            Matricula = matricula;
+            Sueldo = sueldo;
+        }
+    }
+}
+
